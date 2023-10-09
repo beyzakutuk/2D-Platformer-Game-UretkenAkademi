e24982d8e6c95ede21150bc0fd4d1a8e3634561e
Beyza Kütük <beyzakutuk55@gmail.com>
2023-10-09 04:00:29 +0300

    Add Coin.cs

--- Bitirme/Assets/Scripts/Coin.cs
+++ Bitirme/Assets/Scripts/Coin.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
 
     public int coinScore = 0;
 
+    public AudioClip scoreClip;
+    private AudioSource audioSource;
+
+    public GameObject WinPanel;
+
     void Start()
     {
-        
+        PlayerPrefs.SetInt("Coin", coinScore);
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        if(PlayerPrefs.GetInt("Coin") == 34)
+        {
+            WinPanel.gameObject.SetActive(true);
+        }
         
     }
 
@@ -21,7 +32,11 @@ public class Coin : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Coin"))
         {
+            coinScore++;
 
+            audioSource.PlayOneShot(scoreClip);
+            Destroy(collision.gameObject);
+            PlayerPrefs.SetInt("Coin", coinScore);
         }
     }
 }
